01a7e2cb368dbea6e69034a2949a2d040572fda3
Pawan Yadav <78226943+paawanyadav@users.noreply.github.com>
2022-03-15 08:29:28 +0530

    Add files via upload

---
+++ terraform.tfvars
@@ -0,0 +1,4 @@
+ports         = [22, 80, 443, 27017, 3306]
+ami           = "ami-04505e74c0741db8d"
+instance_type = "t2.micro"
+
---
+++ variable.tf
@@ -0,0 +1,10 @@
+variable "ports" {
+  type = list(number)
+}
+variable "ami" {
+  type = string
+}
+variable "instance_type" {
+  type = string
+}
+
